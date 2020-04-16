47a663faa8f6185971c7a10636083c16383c2077
Emanuel <emorcillo@despegar.com>
2020-04-16 16:30:55 -0300

    intento-1

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,33 +1,74 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class Percolation {
+    private String neasd;
 
-    // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) {}
+    private WeightedQuickUnionUF grid;
+    private boolean[][] openedSpaces;
+    private int side;
+    private int top = 0;
+    private int bottom;
 
-    // opens the site (row, col) if it is not open already
-    public void open(int row, int col) {}
-
-    // is the site (row, col) open?
-    public boolean isOpen(int row, int col) {
-        return true;
+    public Percolation(int N) {
+        side = N; //30
+        bottom = side * side + 1; // 901
+        grid = new WeightedQuickUnionUF(size * size + 2); //902
+        opened = new boolean[side][side]; // 30x30
     }
 
-    // is the site (row, col) full?
-    public boolean isFull(int row, int col) {
-        return true;
+
+    public void open(int i, int j) {
+
+        openedSpaces[i - 1][j - 1] = true;
+
+        if (i == 1) {
+            grid.union(getGridIndex(i, j), top);
+        }
+        if (i == size) {
+            grid.union(getGridIndex(i, j), bottom);
+        }
+
+        if (j > 1 && isOpen(i, j - 1)) {
+            grid.union(getGridIndex(i, j), getGridIndex(i, j - 1));
+        }
+        if (j < size && isOpen(i, j + 1)) {
+            grid.union(getGridIndex(i, j), getGridIndex(i, j + 1));
+        }
+        if (i > 1 && isOpen(i - 1, j)) {
+            grid.union(getGridIndex(i, j), getGridIndex(i - 1, j));
+        }
+        if (i < size && isOpen(i + 1, j)) {
+            grid.union(getGridIndex(i, j), getGridIndex(i + 1, j));
+        }
     }
 
-    // returns the number of open sites
-    public int numberOfOpenSites() {
-        return 0;
+    /**
+     * Is site (row i, column j) open?
+     */
+    public boolean isOpen(int i, int j) {
+        return openedSpaces[i - 1][j - 1];
     }
 
-    // does the system percolate?
+    /**
+     * Is site (row i, column j) full?
+     */
+    public boolean isFull(int i, int j) {
+        if (0 < i && i <= size && 0 < j && j <= size) {
+            return grid.connected(top, getGridIndex(i , j));
+        } else {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    /**
+     * Does the system percolate?
+     */
     public boolean percolates() {
-        return true;
+        return grid.connected(top, bottom);
     }
 
-    // test client (optional)
-    public static void main(String[] args) {
-        System.out.println("salu3");
+    private int getGridIndex(int i, int j) {
+        return size * (i - 1) + j;
     }
 }
