362c7c6867617ede6d3ba1144d4302ec49513b61
Emanuel <emorcillo@despegar.com>
2020-04-23 10:39:19 -0300

    s

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -5,14 +5,16 @@ public class PercolationStats {
 
     private double[] computations;
 
+    // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
         this.computations = new double[trials];
         for (int i = 0; i < trials; i++) {
             Percolation trial = percolationTrial(n);
-            this.computations[i] = ((float) trial.numberOfOpenSites()) / n*n;
+            this.computations[i] = (float)trial.numberOfOpenSites() / (n*n);
         }
     }
 
+    // perform one trial on an n-by-n grid
     public Percolation percolationTrial(int n) {
         Percolation p = new Percolation(n);
 
@@ -22,37 +24,41 @@ public class PercolationStats {
                 nextRow = StdRandom.uniform(n);
                 nextCol = StdRandom.uniform(n);
             } while (p.isOpen(nextRow, nextCol));
-
             p.open(nextRow, nextCol);
         }
         return p;
     }
 
+    // sample mean of percolation threshold
     public double mean() {
         return StdStats.mean(this.computations);
     }
 
+    // sample standard deviation of percolation threshold
     public double stddev() {
         return StdStats.stddev(this.computations);
     }
 
+    // low endpoint of 95% confidence interval
     public double confidenceLo() {
         return mean() - (1.96 * stddev() / Math.sqrt(computations.length));
     }
 
+    // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return mean() - (1.96 * stddev() / Math.sqrt(computations.length));
+        return mean() + (1.96 * stddev() / Math.sqrt(computations.length));
     }
 
+    // test client (see below)
     public static void main(String[] args) {
         PercolationStats ps = new PercolationStats(
                 Integer.valueOf(args[0]),
                 Integer.valueOf(args[1]));
 
         System.out.printf(
-                "mean\t\t\t= %f\n" +
-                "stddev\t\t\t= %f\n" +
-                "95%% confidence interval = [%f, %f]\n",
+                "mean\t\t\t= %f%n" +
+                        "stddev\t\t\t= %f%n" +
+                        "95%% confidence interval = [%f, %f]%n",
                 ps.mean(), ps.stddev(), ps.confidenceLo(), ps.confidenceHi());
     }
 }
