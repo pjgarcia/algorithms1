aa69ac0b0405ac197a6f773c8adb11ff3699d074
Emanuel <emorcillo@despegar.com>
2020-04-28 19:45:56 -0300

    ejercicio 1

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,45 +1,51 @@
-
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private WeightedQuickUnionUF grid;
+    private final WeightedQuickUnionUF grid;
     private boolean[][] openedSpaces;
-    private int side;
-    private int top = 0;
-    private int bottom;
-    private int opened;
+    private final int side;
+    private final int bottom;
+    private final int TOP;
 
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int N) {
-        side = N;
-        bottom = side * side + 1;
+    public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException("index " + n + " is > than 1 ");
+        side = n;
+        TOP = (side * side);
+        bottom = (side * side) + 1;
         grid = new WeightedQuickUnionUF(side * side + 2);
-        openedSpaces = new boolean[side+1][side+1];
+        openedSpaces = new boolean[side][side];
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int i, int j) {
+        this.validate(i);
+        this.validate(j);
 
-        openedSpaces[i][j] = true;
+        openedSpaces[i - 1][j - 1] = true;
 
-        if (i == 0) {
-            grid.union(getGridIndex(i, j), top);
+        // top
+        if (i == 1) {
+            grid.union(getGridIndex(i, j), TOP);
         }
-        if (i == side-1) {
+        // bottom
+        if (i == side) {
             grid.union(getGridIndex(i, j), bottom);
         }
-
-        if (i > 0 && isOpen(i - 1, j)) {
+        // upper
+        if (i > 1 && i <= side && isOpen(i - 1, j)) {
             grid.union(getGridIndex(i, j), getGridIndex(i - 1, j));
         }
-        if (i < side && isOpen(i + 1, j)) {
+        // down
+        if (i >= 1 && i < side && isOpen(i + 1, j)) {
             grid.union(getGridIndex(i, j), getGridIndex(i + 1, j));
         }
-
-        if (j > 0 && isOpen(i, j - 1)) {
+        // left <-
+        if (j > 1 && isOpen(i, j - 1)) {
             grid.union(getGridIndex(i, j), getGridIndex(i, j - 1));
         }
+        // right ->
         if (j < side && isOpen(i, j + 1)) {
             grid.union(getGridIndex(i, j), getGridIndex(i, j + 1));
         }
@@ -47,28 +53,47 @@ public class Percolation {
 
     // is the site (row, col) open?
     public boolean isOpen(int i, int j) {
-        return openedSpaces[i][j];
+        validate(i);
+        validate(j);
+        return openedSpaces[i - 1][j - 1];
     }
 
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        for (int i = 0; i < side ; i++) {
+        int opened = 0;
+        for (int i = 0; i < side; i++) {
             for (int j = 0; j < side; j++) {
-                if(openedSpaces[i][j]){
-                    opened ++;
+                if (openedSpaces[i][j]) {
+                    opened++;
                 }
             }
         }
         return opened;
     }
 
-    //Does the system percolate?
+    // is the site (row, col) full?
+    public boolean isFull(int i, int j) {
+        if (0 < i && i <= side && 0 < j && j <= side) {
+            return grid.find(TOP) == grid.find(getGridIndex(i, j));
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // DOes the system percolate?
     public boolean percolates() {
-        return grid.connected(top, bottom);
+        return grid.find(TOP) == grid.find(bottom);
     }
 
     private int getGridIndex(int i, int j) {
-        return side * i  + j;
+        return side * (i - 1) + (j - 1);
+    }
+
+    private void validate(int p) {
+        int n = side;
+        if (p < 1 || p > n) {
+            throw new IllegalArgumentException("index " + p + " is not between 1 and " + (n));
+        }
     }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -3,19 +3,21 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-    private double[] computations;
+    private final double[] computations;
+    private static final double CONFIDENCE_95 = 1.98;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
+        if (n < 0) throw new IllegalArgumentException("index " + n + " is > than 0 ");
         this.computations = new double[trials];
         for (int i = 0; i < trials; i++) {
             Percolation trial = percolationTrial(n);
-            this.computations[i] = (float)trial.numberOfOpenSites() / (n*n);
+            this.computations[i] = (double) trial.numberOfOpenSites() / (n * n);
         }
     }
 
     // perform one trial on an n-by-n grid
-    public Percolation percolationTrial(int n) {
+    private Percolation percolationTrial(int n) {
         Percolation p = new Percolation(n);
 
         while (!p.percolates()) {
@@ -23,8 +25,8 @@ public class PercolationStats {
             do {
                 nextRow = StdRandom.uniform(n);
                 nextCol = StdRandom.uniform(n);
-            } while (p.isOpen(nextRow, nextCol));
-            p.open(nextRow, nextCol);
+            } while (p.isOpen(nextRow + 1, nextCol + 1));
+            p.open(nextRow + 1, nextCol + 1);
         }
         return p;
     }
@@ -41,19 +43,19 @@ public class PercolationStats {
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        return mean() - (1.96 * stddev() / Math.sqrt(computations.length));
+        return mean() - (CONFIDENCE_95 * stddev() / Math.sqrt(computations.length));
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        return mean() + (1.96 * stddev() / Math.sqrt(computations.length));
+        return mean() + (CONFIDENCE_95 * stddev() / Math.sqrt(computations.length));
     }
 
     // test client (see below)
     public static void main(String[] args) {
         PercolationStats ps = new PercolationStats(
-                Integer.valueOf(args[0]),
-                Integer.valueOf(args[1]));
+                Integer.parseInt(args[0]),
+                Integer.parseInt(args[1]));
 
         System.out.printf(
                 "mean\t\t\t= %f%n" +
